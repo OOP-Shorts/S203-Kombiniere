5008c87fc4be346e7d9dd3d8c8794cc4158a1e5d
Alexander Bazo <alexanderbazo@googlemail.com>
2021-12-24 07:46:06 +0100

    Add solution

--- src/Kombinieren.java
+++ src/Kombinieren.java
@@ -1,6 +1,21 @@
+import java.util.Random;
+
 public class Kombinieren {
 
-    public static void main(String[] args) {
+    private static void multiply(int[] a, int[] b) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i] * b[i]);
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        int[] b = new int[5];
+        Random random = new Random();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = random.nextInt();
+            b[i] = random.nextInt();
+        }
+        multiply(a, b);
     }
 }
